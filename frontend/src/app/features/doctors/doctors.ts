a9ef36d848b26a7b5c199d548f446c2d95ff14cf
lamis-iberrakene <iberlamis79@gmail.com>
2026-02-18 15:44:05 +0100

    update doctor profil

--- frontend/src/app/features/doctors/doctors.ts
+++ frontend/src/app/features/doctors/doctors.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-doctors',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './doctors.html',
   styleUrl: './doctors.scss',
 })
 export class Doctors {
+
+  editMode = false;
+
+  doctor = {
+    firstName: 'Sophie',
+    lastName: 'Leclerc',
+    email: 'sophie.leclerc@email.com',
+    phone: '0612345678',
+    speciality: 'Cardiologie',
+    description: "Médecin cardiologue avec 10 ans d'expérience."
+  };
+
+  toggleEdit() {
+    this.editMode = !this.editMode;
+  }
+
+  save() {
+    this.editMode = false;
+    console.log('Saved doctor:', this.doctor);
+  }
 
 }
