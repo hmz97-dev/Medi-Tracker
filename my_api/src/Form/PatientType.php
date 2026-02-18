fc20ca9d55d2ac50fba931e7f0980c144b7e9f77
Hamza <bdhh1112@gmail.com>
2026-02-18 09:59:41 +0100

    implement crud operations for Patient entity

---
+++ my_api/src/Form/PatientType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Patient;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PatientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('FirstName')
+            ->add('LastName')
+            ->add('dateNaissance')
+            ->add('Gender')
+            ->add('Email')
+            ->add('Description')
+            ->add('Adress')
+            ->add('BloodGroup')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Patient::class,
+        ]);
+    }
+}
